fd72db8120e435e892c63ce2b845bb95daaeee93
Michael Chown <michaelchown08@gmail.com>
2024-01-21 13:21:29 +0700

    crawl single page with error handling

--- crawl.js
+++ crawl.js
@@ -1,43 +1,57 @@
-const { link } = require("fs")
-const { url } = require("inspector")
-const {JSDOM} = require('jsdom')
-const { hrtime } = require("process")
+const { JSDOM } = require('jsdom')
 
-function getURLsFromHTML(htmlBody, baseURL){
-    const urls = []
-    const dom = new JSDOM(htmlBody)
-    const linkElements = dom.window.document.querySelectorAll('a')
-    for (const linkElement of linkElements){
-        if (linkElement.href.slice(0, 1) === '/'){
-            //relative
-            try {
-                const urlObj = new URL(`${baseURL}${linkElement.href}`)
-            urls.push(urlObj.href)
-            } catch (err){
-                console.log(`error with relative url; ${err.message}`)
-            }
-        } else {
-            try {
-                const urlObj = new URL(linkElement.href)
-            urls.push(urlObj.href)
-            } catch (err){
-                console.log(`error with absolute url; ${err.message}`)
-            }
-        }
+async function crawlPage(currentURL){
+  console.log(`crawling ${currentURL}`)
+  try {
+    const resp = await fetch(currentURL)
+    if (resp.status > 399){
+      console.log(`Got HTTP error, status code: ${resp.status}`)
+      return
     }
-    return urls
+    const contentType = resp.headers.get('content-type')
+    if (!contentType.includes('text/html')){
+      console.log(`Got non-html response: ${contentType}`)
+      return
+    }
+    console.log(await resp.text())
+  } catch (err){
+    console.log(err.message)
+  }
 }
 
-function normalizeURL(urlString){
-    const urlObj = new URL(urlString)
-    const hostPath = `${urlObj.hostname}${urlObj.pathname}`
-    if (hostPath.length > 0 && hostPath.slice(-1) === '/'){
-        return hostPath.slice(0, -1)
+function getURLsFromHTML(htmlBody, baseURL){
+  const urls = []
+  const dom = new JSDOM(htmlBody)
+  const aElements = dom.window.document.querySelectorAll('a')
+  for (const aElement of aElements){
+    if (aElement.href.slice(0,1) === '/'){
+      try {
+        urls.push(new URL(aElement.href, baseURL).href)
+      } catch (err){
+        console.log(`${err.message}: ${aElement.href}`)
+      }
+    } else {
+      try {
+        urls.push(new URL(aElement.href).href)
+      } catch (err){
+        console.log(`${err.message}: ${aElement.href}`)
+      }
     }
-    return hostPath
+  }
+  return urls
+}
+
+function normalizeURL(url){
+  const urlObj = new URL(url)
+  let fullPath = `${urlObj.host}${urlObj.pathname}`
+  if (fullPath.length > 0 && fullPath.slice(-1) === '/'){
+    fullPath = fullPath.slice(0, -1)
+  }
+  return fullPath
 }
 
 module.exports = {
-    normalizeURL,
-    getURLsFromHTML
+  crawlPage,
+  normalizeURL,
+  getURLsFromHTML
 }
